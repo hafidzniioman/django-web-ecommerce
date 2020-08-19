84d72792bda12ff14b7a45f8197bf21466c9c0e0
hafidzniioman <hafidzniioman@gmail.com>
2020-08-19 23:12:38 +0700

    add user interest and render to profile view

--- inventory_website/apps/accounts/models.py
+++ inventory_website/apps/accounts/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+class UserInterest(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    normalized_name = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class UserPersona(models.Model):
     name = models.CharField(max_length=64, unique=True)
     normalized_name = models.CharField(max_length=64, unique=True)
@@ -19,3 +27,5 @@ class UserProfile(models.Model):
     bio = models.CharField(max_length=500, blank=True, null=True)
     website = models.URLField(max_length=200, blank=True, null=True)
     persona = models.ForeignKey(UserPersona, on_delete=models.SET_NULL, blank=True, null=True)
+    interest = models.ManyToManyField(UserInterest, blank=True)
+
